9996525f1eefd583c61c37078d6a6377be45a8e0
lixiaofly <lixiao_fly@126.com>
2020-04-17 19:37:02 +0800

    Update oss_config.c

--- oss_c_sdk.bak/sample/oss_config.c
+++ oss_c_sdk.bak/sample/oss_config.c
@@ -2,6 +2,6 @@
 
 const char TEST_OSS_HOST[] = "oss-cn-hangzhou-internal.aliyuncs.com";
 const int TEST_OSS_PORT = 80;
-const char TEST_ACCESS_ID[] = "WRIBSFML486WciQr";
-const char TEST_ACCESS_KEY[] = "HY5YRv4cfqmbaOmf8JwIjFxPmtootb";
-const char TEST_BUCKET_NAME[] = "ums_autelan_com";
+const char TEST_ACCESS_ID[] = "";
+const char TEST_ACCESS_KEY[] = "";
+const char TEST_BUCKET_NAME[] = "";
